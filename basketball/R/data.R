137b6a309fc56329c6ffd157cb15d9a3afe084e0
Chiamaka Azodo <90811309+chiamakaazodo@users.noreply.github.com>
2022-03-24 01:16:22 -0400

    added nba data and documentation

---
+++ basketball/R/data.R
@@ -0,0 +1,11 @@
+#' Data that contains the aggregate statistics of 67 NBA seasons
+#'
+#' Contains 53 player statistics from the NBA seasons
+#'
+#' @format A data set with 24,691 rows and 53 variables
+#'
+#' @source https://www.kaggle.com/drgilermo/nba-players-stats/data?select=Seasons_Stats.csv
+#'
+#' @examples
+#' data(nba)
+"nba"
